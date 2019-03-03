0811221eb87d7ef197ef7dec88599a7eb7d92095
J. Protopopovs <jprotopopov1122@gmail.com>
2019-03-03 19:33:08 +0200

    Add code generation tests

--- tests/runtime/Translator.cpp
+++ tests/runtime/Translator.cpp
@@ -6,7 +6,7 @@ using namespace GCodeLib::Runtime;
 using NodeList = std::vector<std::unique_ptr<GCodeNode>>;
 
 class GCodeTestMangler : public GCodeNameMangler {
-
+ public:
   std::string getProcedureName(const std::string &proc) const override {
     return "procedure_" + proc;
   }
@@ -43,16 +43,23 @@ struct MakeAstImpl<A, B...>  {
   };
 };
 
-template <class NodeType, typename ... T>
-static std::unique_ptr<GCodeBlock> make_node_list(T &&... args) {
+template <typename ... T>
+static std::vector<std::unique_ptr<GCodeNode>> make_node_list(T &&... args) {
   std::vector<std::unique_ptr<GCodeNode>> vec;
   MakeAstImpl<T...>::push(vec, std::forward<T>(args)...);
-  return std::make_unique<NodeType>(std::move(vec), position);
+  return vec;
 }
 
 template <typename ... T>
-static auto make_ast(T &&... args) {
-  return make_node_list<GCodeBlock, T...>(std::forward<T>(args)...);
+static std::unique_ptr<GCodeBlock> make_ast(T &&... args) {
+  std::vector<std::unique_ptr<GCodeNode>> vec;
+  MakeAstImpl<T...>::push(vec, std::forward<T>(args)...);
+  return std::make_unique<GCodeBlock>(std::move(vec), position);
+}
+
+template <typename T>
+static std::unique_ptr<T> make_node(T &&arg) {
+  return std::make_unique<T>(std::move(arg));
 }
 
 static bool operator!=(const GCodeRuntimeValue &v1, const GCodeRuntimeValue &v2) {
@@ -86,17 +93,31 @@ static bool verify_ir(GCodeIRModule &module, std::initializer_list<GCodeIRInstru
   return true;
 }
 
+static constexpr int64_t IntegerConstants[] = {
+  100,
+  200,
+  300
+};
+
+static constexpr double FloatConstants[] = {
+  3.14
+};
+
+static constexpr const char *StringConstants[] = {
+  "Hello"
+};
+
 TEST_CASE("Constants") {
   GCodeIRTranslator translator(mangler);
-  auto ast = make_ast(GCodeConstantValue(100L, position), GCodeConstantValue(3.14, position), GCodeConstantValue("Hello", position));
+  auto ast = make_ast(GCodeConstantValue(IntegerConstants[0], position),
+    GCodeConstantValue(FloatConstants[0], position),
+    GCodeConstantValue(StringConstants[0], position));
   auto ir = translator.translate(*ast);
-  REQUIRE(ir->length() == 3);
-  REQUIRE(ir->at(0).getOpcode() == GCodeIROpcode::Push);
-  REQUIRE(ir->at(0).getValue().getInteger() == 100);
-  REQUIRE(ir->at(1).getOpcode() == GCodeIROpcode::Push);
-  REQUIRE(ir->at(1).getValue().getFloat() == 3.14);
-  REQUIRE(ir->at(2).getOpcode() == GCodeIROpcode::Push);
-  REQUIRE(ir->at(2).getValue().getString().compare("Hello") == 0);
+  REQUIRE(verify_ir(*ir, {
+    { GCodeIROpcode::Push, IntegerConstants[0] },
+    { GCodeIROpcode::Push, FloatConstants[0] },
+    { GCodeIROpcode::Push, std::string(StringConstants[0]) }
+  }));
 }
 
 TEST_CASE("Numbered variables") {
@@ -104,9 +125,9 @@ TEST_CASE("Numbered variables") {
   const int ID = 1;
   auto ast = make_ast(GCodeNumberedVariable(ID, position));
   auto ir = translator.translate(*ast);
-  REQUIRE(ir->length() == 1);
-  REQUIRE(ir->at(0).getOpcode() == GCodeIROpcode::LoadNumbered);
-  REQUIRE(ir->at(0).getValue().getInteger() == ID);
+  REQUIRE(verify_ir(*ir, {
+    { GCodeIROpcode::LoadNumbered, static_cast<int64_t>(ID) }
+  }));
 }
 
 TEST_CASE("Named variables") {
@@ -122,10 +143,10 @@ TEST_CASE("Named variables") {
 TEST_CASE("Numbered variable assignment") {
   GCodeIRTranslator translator(mangler);
   const int64_t ID = 1;
-  auto ast = make_ast(GCodeNumberedVariableAssignment(ID, std::make_unique<GCodeConstantValue>(100L, position), position));
+  auto ast = make_ast(GCodeNumberedVariableAssignment(ID, std::make_unique<GCodeConstantValue>(IntegerConstants[0], position), position));
   auto ir = translator.translate(*ast);
   REQUIRE(verify_ir(*ir, {
-    { GCodeIROpcode::Push, 100L },
+    { GCodeIROpcode::Push, IntegerConstants[0] },
     { GCodeIROpcode::StoreNumbered, ID }
   }));
 }
@@ -133,17 +154,17 @@ TEST_CASE("Numbered variable assignment") {
 TEST_CASE("Named variable assignment") {
   GCodeIRTranslator translator(mangler);
   const std::string &ID = "abc";
-  auto ast = make_ast(GCodeNamedVariableAssignment(ID, std::make_unique<GCodeConstantValue>(100L, position), position));
+  auto ast = make_ast(GCodeNamedVariableAssignment(ID, std::make_unique<GCodeConstantValue>(IntegerConstants[0], position), position));
   auto ir = translator.translate(*ast);
   REQUIRE(verify_ir(*ir, {
-    { GCodeIROpcode::Push, 100L },
+    { GCodeIROpcode::Push, IntegerConstants[0] },
     { GCodeIROpcode::StoreNamed, static_cast<int64_t>(ir->getSymbolId(ID)) }
   }));
 }
 
 TEST_CASE("Unary operations") {
   GCodeIRTranslator translator(mangler);
-  GCodeConstantValue arg(100L, position);
+  GCodeConstantValue arg(IntegerConstants[0], position);
   std::map<GCodeUnaryOperation::Operation, GCodeIROpcode> ops = {
     { GCodeUnaryOperation::Operation::Negate, GCodeIROpcode::Negate }
   };
@@ -151,7 +172,7 @@ TEST_CASE("Unary operations") {
     auto ast = make_ast(GCodeUnaryOperation(kv.first, std::make_unique<GCodeConstantValue>(arg), position));
     auto ir = translator.translate(*ast);
     REQUIRE(verify_ir(*ir, {
-      { GCodeIROpcode::Push, 100L },
+      { GCodeIROpcode::Push, IntegerConstants[0] },
       { kv.second }
     }));
   }
@@ -159,26 +180,268 @@ TEST_CASE("Unary operations") {
 
 TEST_CASE("Binary operations") {
   GCodeIRTranslator translator(mangler);
-  GCodeConstantValue left(100L, position);
-  GCodeConstantValue right(200L, position);
-  std::map<GCodeBinaryOperation::Operation, GCodeIROpcode> ops = {
-    { GCodeBinaryOperation::Operation::Add, GCodeIROpcode::Add },
-    { GCodeBinaryOperation::Operation::Subtract, GCodeIROpcode::Subtract },
-    { GCodeBinaryOperation::Operation::Multiply, GCodeIROpcode::Multiply },
-    { GCodeBinaryOperation::Operation::Divide, GCodeIROpcode::Divide },
-    { GCodeBinaryOperation::Operation::Power, GCodeIROpcode::Power },
-    { GCodeBinaryOperation::Operation::Modulo, GCodeIROpcode::Modulo },
-    { GCodeBinaryOperation::Operation::And, GCodeIROpcode::And },
-    { GCodeBinaryOperation::Operation::Or, GCodeIROpcode::Or },
-    { GCodeBinaryOperation::Operation::Xor, GCodeIROpcode::Xor },
-  };
-  for (const auto &kv : ops) {
-    auto ast = make_ast(GCodeBinaryOperation(kv.first, std::make_unique<GCodeConstantValue>(left), std::make_unique<GCodeConstantValue>(right), position));
+  GCodeConstantValue left(IntegerConstants[0], position);
+  GCodeConstantValue right(IntegerConstants[2], position);
+  SECTION("Arithmetic") {
+    std::map<GCodeBinaryOperation::Operation, GCodeIROpcode> ops = {
+      { GCodeBinaryOperation::Operation::Add, GCodeIROpcode::Add },
+      { GCodeBinaryOperation::Operation::Subtract, GCodeIROpcode::Subtract },
+      { GCodeBinaryOperation::Operation::Multiply, GCodeIROpcode::Multiply },
+      { GCodeBinaryOperation::Operation::Divide, GCodeIROpcode::Divide },
+      { GCodeBinaryOperation::Operation::Power, GCodeIROpcode::Power },
+      { GCodeBinaryOperation::Operation::Modulo, GCodeIROpcode::Modulo },
+      { GCodeBinaryOperation::Operation::And, GCodeIROpcode::And },
+      { GCodeBinaryOperation::Operation::Or, GCodeIROpcode::Or },
+      { GCodeBinaryOperation::Operation::Xor, GCodeIROpcode::Xor },
+    };
+    for (const auto &kv : ops) {
+      auto ast = make_ast(GCodeBinaryOperation(kv.first, std::make_unique<GCodeConstantValue>(left), std::make_unique<GCodeConstantValue>(right), position));
+      auto ir = translator.translate(*ast);
+      REQUIRE(verify_ir(*ir, {
+        { GCodeIROpcode::Push, IntegerConstants[0] },
+        { GCodeIROpcode::Push, IntegerConstants[2] },
+        { kv.second }
+      }));
+    }
+  }
+  SECTION("Comparison") {
+    std::map<GCodeBinaryOperation::Operation, int64_t> ops = {
+      { GCodeBinaryOperation::Operation::Equals, static_cast<int64_t>(GCodeCompare::Equals) },
+      { GCodeBinaryOperation::Operation::NotEquals, static_cast<int64_t>(GCodeCompare::NotEquals) },
+      { GCodeBinaryOperation::Operation::Greater, static_cast<int64_t>(GCodeCompare::Greater) },
+      { GCodeBinaryOperation::Operation::GreaterOrEquals, static_cast<int64_t>(GCodeCompare::Greater) | static_cast<int64_t>(GCodeCompare::Equals) },
+      { GCodeBinaryOperation::Operation::Lesser, static_cast<int64_t>(GCodeCompare::Lesser) },
+      { GCodeBinaryOperation::Operation::LesserOrEquals, static_cast<int64_t>(GCodeCompare::Lesser) | static_cast<int64_t>(GCodeCompare::Equals) }
+    };
+    for (const auto &kv : ops) {
+      auto ast = make_ast(GCodeBinaryOperation(kv.first, std::make_unique<GCodeConstantValue>(left), std::make_unique<GCodeConstantValue>(right), position));
+      auto ir = translator.translate(*ast);
+      REQUIRE(verify_ir(*ir, {
+        { GCodeIROpcode::Push, IntegerConstants[0] },
+        { GCodeIROpcode::Push, IntegerConstants[2] },
+        { GCodeIROpcode::Compare },
+        { GCodeIROpcode::Test, kv.second }
+      }));
+    }
+  }
+}
+
+TEST_CASE("Procedure return") {
+  GCodeIRTranslator translator(mangler);
+  auto ast = make_ast(GCodeProcedureReturn(make_node_list(
+    GCodeConstantValue(IntegerConstants[0], position),
+    GCodeConstantValue(2.718, position)
+  ), position));
+  auto ir = translator.translate(*ast);
+  REQUIRE(verify_ir(*ir, {
+    { GCodeIROpcode::Push, IntegerConstants[0] },
+    { GCodeIROpcode::Push, 2.718 },
+    { GCodeIROpcode::Ret, 2L }
+  }));
+}
+
+TEST_CASE("Procedure definition") {
+  GCodeIRTranslator translator(mangler);
+  const int ID = 0;
+  auto ast = make_ast(GCodeProcedureDefinition(ID,
+    make_ast(GCodeConstantValue(IntegerConstants[0], position)),
+    make_node_list(GCodeConstantValue(IntegerConstants[2], position)),
+    position));
+  auto ir = translator.translate(*ast);
+  REQUIRE(verify_ir(*ir, {
+    { GCodeIROpcode::Jump, 4L },
+    { GCodeIROpcode::Push, IntegerConstants[0] },
+    { GCodeIROpcode::Push, IntegerConstants[2] },
+    { GCodeIROpcode::Ret, 1L }
+  }));
+  REQUIRE(ir->getProcedure(ID).getAddress() == 1);
+}
+
+TEST_CASE("Procedure call") {
+  GCodeIRTranslator translator(mangler);
+  const int64_t ID = 0;
+  auto ast = make_ast(GCodeProcedureCall(
+    make_ast(GCodeConstantValue(ID, position)),
+    make_node_list(GCodeConstantValue(IntegerConstants[0], position), GCodeConstantValue(IntegerConstants[1], position)),
+    position));
+  auto ir = translator.translate(*ast);
+  REQUIRE(verify_ir(*ir, {
+    { GCodeIROpcode::Push, IntegerConstants[0] },
+    { GCodeIROpcode::Push, IntegerConstants[1] },
+    { GCodeIROpcode::Push, ID },
+    { GCodeIROpcode::Call, 2L }
+  }));
+}
+
+TEST_CASE("Function call") {
+  GCodeIRTranslator translator(mangler);
+  auto ast = make_ast(GCodeFunctionCall(
+    StringConstants[0],
+    make_node_list(GCodeConstantValue(IntegerConstants[0], position), GCodeConstantValue(IntegerConstants[1], position)),
+    position
+  ));
+  auto ir = translator.translate(*ast);
+  REQUIRE(verify_ir(*ir, {
+    { GCodeIROpcode::Push, IntegerConstants[1] },
+    { GCodeIROpcode::Push, IntegerConstants[0] },
+    { GCodeIROpcode::Push, 2L },
+    { GCodeIROpcode::Invoke, static_cast<int64_t>(ir->getSymbolId(StringConstants[0])) }
+  }));
+}
+
+TEST_CASE("Conditionals") {
+  GCodeIRTranslator translator(mangler);
+  SECTION("If-Else") {
+    auto ast = make_ast(GCodeConditional(
+      make_node(GCodeConstantValue(IntegerConstants[0], position)),
+      make_node(GCodeConstantValue(IntegerConstants[1], position)),
+      make_node(GCodeConstantValue(IntegerConstants[2], position)),
+      position));
     auto ir = translator.translate(*ast);
     REQUIRE(verify_ir(*ir, {
-      { GCodeIROpcode::Push, 100L },
-      { GCodeIROpcode::Push, 200L },
-      { kv.second }
+      { GCodeIROpcode::Push, IntegerConstants[0] },
+      { GCodeIROpcode::Not },
+      { GCodeIROpcode::JumpIf, 5L },
+      { GCodeIROpcode::Push, IntegerConstants[1] },
+      { GCodeIROpcode::Jump, 6L },
+      { GCodeIROpcode::Push, IntegerConstants[2] }
     }));
   }
+  SECTION("If") {
+    auto ast = make_ast(GCodeConditional(
+      make_node(GCodeConstantValue(IntegerConstants[0], position)),
+      make_node(GCodeConstantValue(IntegerConstants[1], position)),
+      nullptr,
+      position));
+    auto ir = translator.translate(*ast);
+    REQUIRE(verify_ir(*ir, {
+      { GCodeIROpcode::Push, IntegerConstants[0] },
+      { GCodeIROpcode::Not },
+      { GCodeIROpcode::JumpIf, 4L },
+      { GCodeIROpcode::Push, IntegerConstants[1] },
+    }));
+  }
+}
+
+TEST_CASE("While loop") {
+  const int64_t ID = 0;
+  std::string loopName = mangler.GCodeNameMangler::getLoop(ID);
+  GCodeIRTranslator translator(mangler);
+  SECTION("While loop") {
+    auto ast = make_ast(GCodeWhileLoop(
+      ID,
+      make_node(GCodeConstantValue(IntegerConstants[0], position)),
+      make_node(GCodeConstantValue(IntegerConstants[1], position)),
+      false,
+      position));
+    auto ir = translator.translate(*ast);
+    REQUIRE(verify_ir(*ir, {
+      { GCodeIROpcode::Jump, 2L },
+      { GCodeIROpcode::Push, IntegerConstants[1] },
+      { GCodeIROpcode::Push, IntegerConstants[0] },
+      { GCodeIROpcode::JumpIf, 1L }
+    }));
+    REQUIRE(ir->getNamedLabel(mangler.getStatementStart(loopName)).getAddress() == 2);
+    REQUIRE(ir->getNamedLabel(mangler.getStatementEnd(loopName)).getAddress() == 4); 
+  }
+  SECTION("Do-While loop") {
+    auto ast = make_ast(GCodeWhileLoop(
+      0,
+      make_node(GCodeConstantValue(IntegerConstants[0], position)),
+      make_node(GCodeConstantValue(IntegerConstants[1], position)),
+      true,
+      position));
+    auto ir = translator.translate(*ast);
+    REQUIRE(verify_ir(*ir, {
+      { GCodeIROpcode::Push, IntegerConstants[1] },
+      { GCodeIROpcode::Push, IntegerConstants[0] },
+      { GCodeIROpcode::JumpIf, 0L }
+    }));
+    REQUIRE(ir->getNamedLabel(mangler.getStatementStart(loopName)).getAddress() == 1);
+    REQUIRE(ir->getNamedLabel(mangler.getStatementEnd(loopName)).getAddress() == 3); 
+  }
+}
+
+TEST_CASE("Repeat loop") {
+  const int64_t ID = 0;
+  GCodeIRTranslator translator(mangler);
+  auto ast = make_ast(GCodeRepeatLoop(
+    ID,
+    make_node(GCodeConstantValue(IntegerConstants[0], position)),
+    make_node(GCodeConstantValue(IntegerConstants[1], position)),
+    position));
+  auto ir = translator.translate(*ast);
+  REQUIRE(verify_ir(*ir, {
+    { GCodeIROpcode::Push, IntegerConstants[0] },
+    { GCodeIROpcode::Jump, 5L },
+    { GCodeIROpcode::Push, 1L },
+    { GCodeIROpcode::Subtract },
+    { GCodeIROpcode::Push, IntegerConstants[1] },
+    { GCodeIROpcode::Dup },
+    { GCodeIROpcode::Push, 0L },
+    { GCodeIROpcode::Compare },
+    { GCodeIROpcode::Test, static_cast<int64_t>(GCodeCompare::Greater) },
+    { GCodeIROpcode::JumpIf, 2L }
+  }));
+  std::string loopName = mangler.GCodeNameMangler::getLoop(ID);
+  REQUIRE(ir->getNamedLabel(mangler.getStatementStart(loopName)).getAddress() == 5);
+  REQUIRE(ir->getNamedLabel(mangler.getStatementEnd(loopName)).getAddress() == 10);
+}
+
+TEST_CASE("Named statement") {
+  const std::string &NAME = "test";
+  GCodeIRTranslator translator(mangler);
+  auto ast = make_ast(GCodeNamedStatement(
+    NAME,
+    make_node(GCodeConstantValue(IntegerConstants[0], position)),
+    position));
+  auto ir = translator.translate(*ast);
+  REQUIRE(verify_ir(*ir, {
+    { GCodeIROpcode::Push, IntegerConstants[0] }
+  }));
+  REQUIRE(ir->getNamedLabel(mangler.getStatementStart(NAME)).getAddress() == 0);
+  REQUIRE(ir->getNamedLabel(mangler.getStatementEnd(NAME)).getAddress() == 1);
+}
+
+TEST_CASE("Loop control") {
+  const int64_t ID = 0;
+  GCodeIRTranslator translator(mangler);
+  auto ast = make_ast(GCodeWhileLoop(ID,
+    make_node(GCodeConstantValue(IntegerConstants[0], position)),
+    make_ast(
+      GCodeLoopControl(ID, GCodeLoopControl::ControlType::Continue, position),
+      GCodeLoopControl(ID, GCodeLoopControl::ControlType::Break, position)),
+    true,
+    position));
+  auto ir = translator.translate(*ast);
+  REQUIRE(verify_ir(*ir, {
+    { GCodeIROpcode::Jump, 2L },
+    { GCodeIROpcode::Jump, 4L },
+    { GCodeIROpcode::Push, IntegerConstants[0] },
+    { GCodeIROpcode::JumpIf, 0L }
+  }));
+}
+
+TEST_CASE("Command") {
+  GCodeIRTranslator translator(mangler);
+  std::vector<std::unique_ptr<GCodeWord>> args;
+  args.push_back(make_node(GCodeWord('A', make_node(GCodeConstantValue(IntegerConstants[0], position)), position)));
+  args.push_back(make_node(GCodeWord('B', make_node(GCodeConstantValue(IntegerConstants[1], position)), position)));
+  args.push_back(make_node(GCodeWord('C', make_node(GCodeConstantValue(IntegerConstants[2], position)), position)));
+  auto ast = make_ast(GCodeCommand(
+    make_node(GCodeWord('G', make_node(GCodeConstantValue(IntegerConstants[0], position)), position)),
+    std::move(args),
+    position));
+  auto ir = translator.translate(*ast);
+  REQUIRE(verify_ir(*ir, {
+    { GCodeIROpcode::Prologue },
+    { GCodeIROpcode::Push, IntegerConstants[0] },
+    { GCodeIROpcode::SetArg, static_cast<int64_t>('A') },
+    { GCodeIROpcode::Push, IntegerConstants[1] },
+    { GCodeIROpcode::SetArg, static_cast<int64_t>('B') },
+    { GCodeIROpcode::Push, IntegerConstants[2] },
+    { GCodeIROpcode::SetArg, static_cast<int64_t>('C') },
+    { GCodeIROpcode::Push, IntegerConstants[0] },
+    { GCodeIROpcode::Syscall, static_cast<int64_t>('G') }
+  }));
 }
